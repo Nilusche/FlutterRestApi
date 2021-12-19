7b9eb8dd3bbc9262485b6a32a6afe50868a0aed1
Nilusche <niluschel@outlook.de>
2021-12-19 01:45:59 +0100

    init

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Comment;
+class CommentController extends Controller
+{
+    //get all comments of a post
+
+    public function index($id){
+        $post = Post::find($id);
+        
+        if(!$post){
+            return response([
+                'message' => 'Post not found'
+            ], 403);
+        }
+
+        return response([
+            'comments' => $post->comments()->with('user:id,name,image')->get()
+        ],200);
+    }
+
+    //create a comment 
+    public function store(Request $request, $id){
+        $post = Post::find($id);
+        
+        if(!$post){
+            return response([
+                'message' => 'Post not found'
+            ], 403);
+        }
+
+        $attrs = $request->validate([
+            'comments'=>'required|string'
+        ]);
+
+        Comment::create([
+            'comments' => $attrs['comments'],
+            'post_id' => $id, 
+            'user_id' => auth()->user()->id
+        ]);
+
+        return response([
+            'message' => 'Comment created.'
+        ],200);
+    }
+
+    //update a comment
+
+    public function update(Request $request, $id){
+        $comment = Comment::find($id);
+
+        if(!$comment){
+            return response([
+                'message' => 'Comment not found'
+            ], 403);
+        }
+
+        if(!$comment->user_id==auth()->user()->id){
+            return response([
+                'message' => 'Permission denied'
+            ], 403);
+        }
+
+        $attrs = $request->validate([
+            'comments'=>'required|string'
+        ]);
+
+        $comment->update([
+            'comments' => $attrs['comments']
+        ]);
+
+        return response([
+            'message' => 'Comment updated.'
+        ],200);
+    }
+
+    //delete a comment
+
+    public function destroy($id){
+        $comment = Comment::find($id);
+        if(!$comment){
+            return response([
+                'message' => 'Comment not found'
+            ],403);
+        }
+
+        if(!$comment->user_id==auth()->user()->id){
+            return response([
+                'message' => 'Permission denied'
+            ], 403);
+        }
+
+        $comment->delete();
+        return response([
+            'message' => 'Comment deleted.'
+        ],200);
+    }
+}
